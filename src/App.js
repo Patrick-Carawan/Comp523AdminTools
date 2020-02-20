a6045fc7a7e5d016358d8d2f3eb8dc24801db5bb
DanielWeber97 <daniel.weber.443@gmail.com>
2020-02-20 17:34:18 -0500

    about to pull to override

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        fetch('/users').then(res => res.json()).then(users => setUsers(users))
+    }, []);
+
+    return (
+        <div className="App">
+            <h1>Users</h1>
+            {users.map(user =>
+                <div key={user.id}>{user.username}</div>
+            )}
+        </div>
+    );
 }
 
 export default App;
