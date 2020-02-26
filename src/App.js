b4fb5684931e57f4ec9b9619cfc17f11d73520fc
DanielWeber97 <daniel.weber.443@gmail.com>
2020-02-26 16:40:12 -0500

    set up basic routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
+import {BrowserRouter, Route} from 'react-router-dom';
+import Switch from "react-router-dom/es/Switch";
+import ClientForm from "./Components/ClientForm";
+import Home from "./Components/Home";
 
 function App() {
-    const [users, setUsers] = useState([]);
-    useEffect(() => {
-        fetch('/users').then(res => res.json()).then(users => setUsers(users))
-    }, []);
+
 
     return (
-        <div className="App">
-            <h1>Users</h1>
-            {users.map(user =>
-                <div key={user.id}>{user.username}</div>
-            )}
-        </div>
-    )}
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" component={Home} exact={true}/>
+                <Route path="/ClientForm" component={ClientForm} exact={true}/>
+            </Switch>
+
+        </BrowserRouter>
+    )
+}
+
 export default App;
---
+++ src/Components/Home.jsx
@@ -0,0 +1,19 @@
+import React, {useEffect, useState} from 'react';
+
+function Home(props) {
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        fetch('/users').then(res => res.json()).then(users => setUsers(users))
+    }, []);
+    return (
+        <div className="App">
+            <h1>Welcome to the COMP 523 Admin Tool</h1>
+            <h2>Dummy users pulled from static express:</h2>
+            {users.map(user =>
+                <div key={user.id}>{user.username}</div>
+            )}
+        </div>
+    );
+}
+
+export default Home;
