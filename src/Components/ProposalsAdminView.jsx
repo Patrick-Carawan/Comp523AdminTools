516cb46b3544d423c75942942d8185f93a53ffca
DanielWeber97 <daniel.weber.443@gmail.com>
2020-02-27 18:07:36 -0500

    created Admin view of proposals

---
+++ src/Components/ProposalsAdminView.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const proposals = [{},{},{}]
+
+function ProposalsAdminView(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default ProposalsAdminView;
