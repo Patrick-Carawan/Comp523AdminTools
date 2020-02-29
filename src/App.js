af0545256e6fba5595249936088b240a034c71e6
DanielWeber97 <daniel.weber.443@gmail.com>
2020-02-28 22:20:09 -0500

    added admin page to routes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter, Switch ,Route} from 'react-router-dom';
 import ClientForm from "./Components/ClientForm";
 import Home from "./Components/Home";
 import Login from "./Components/Login";
+import ProposalsAdminView from "./Components/ProposalsAdminView";
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
                 <Route path="/" component={Home} exact={true}/>
                 <Route path="/clientForm" component={ClientForm} exact={true}/>
                 <Route path="/login" component={Login} exact={true}/>
+                <Route path="/admin" component={ProposalsAdminView} exact={true}/>
             </Switch>
 
         </BrowserRouter>
