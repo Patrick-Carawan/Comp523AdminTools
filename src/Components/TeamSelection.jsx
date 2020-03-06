3910c4d7cfd9292717fd9c974daafdd2a07322af
DanielWeber97 <daniel.weber.443@gmail.com>
2020-03-06 03:26:35 -0500

    styling on the team tiles good enough/functional

--- src/Components/TeamSelection.jsx
+++ src/Components/TeamSelection.jsx
@@ -3,9 +3,13 @@ import TeamBox from './TeamBox';
 import Name from './Name';
 import Grid from "@material-ui/core/Grid";
 import {Card, CardContent, Container} from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
+
+//This will need to come from the backend. Teacher should be able to set this parameter.
+const numTeams = 8;
 
 const studentNames = [
-    "Daniel", "Abraham", "Patrick", "Zion",
+    "Daniel Austin Weber Longname", "Abraham", "Patrick", "Zion",
     "Bill", "Jim", "Billy", "Bob",
     "Cory", "Josiah", "Ezekial", "Al",
     "Fred", "Keaton", "Hector", "Susie",
@@ -23,26 +27,39 @@ function TeamSelection(props) {
     return (
         <div>
             <TeamBox id="box-1" className="nameBank">
-                        {/*<Name id="name-1" className="card" draggable="true">*/}
-                        {/*        <p>Card One</p>*/}
-                        {/*</Name>*/}
-                        {studentNames.map((studentName, index) =>
-                                <Name id={index} className="name" draggable="true">
-                                    <Card variant="outlined">
-                                        <CardContent>
-                                            {studentName}
-                                        </CardContent>
+                {/*<Name id="name-1" className="card" draggable="true">*/}
+                {/*        <p>Card One</p>*/}
+                {/*</Name>*/}
+                {studentNames.map((studentName, index) =>
+                    <Name id={index} className="name" draggable="true">
+                        <Card variant="outlined">
+                            <CardContent>
+                                {studentName}
+                            </CardContent>
+                        </Card>
+                    </Name>
+                )}
+            </TeamBox>
+            {/*<TeamBox id="box-2" className="teamTile">*/}
+            {/*    <Name id="name-2" className="card" draggable="true">*/}
+            {/*        <p>Card Two</p>*/}
+            {/*    </Name>*/}
+            {/*</TeamBox>*/}
+            <Container>
+                <Grid container spacing={3}>
+                    {letters.map((letter, index) =>
+                        index < numTeams ?
+                            <Grid item key={index} xs={3} ml={5}>
+                                <TeamBox id={`group${index}`}>
+                                    <Card className="teamTile">
+                                        <Typography variant="h6">Team {letter}</Typography>
                                     </Card>
-                                </Name>
-                        )}
-            </TeamBox>
-            <TeamBox id="box-2" className="teamTile">
-                <Name id="name-2" className="card" draggable="true">
-                    <p>Card Two</p>
-                </Name>
-            </TeamBox>
-
-
+                                </TeamBox>
+                            </Grid>
+                            : <></>
+                    )}
+                </Grid>
+            </Container>
         </div>
     );
 }
