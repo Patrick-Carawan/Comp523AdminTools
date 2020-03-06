88b2ac8c998ea6757bffe3256bff965e3b4c3cc0
DanielWeber97 <daniel.weber.443@gmail.com>
2020-03-06 02:52:42 -0500

    css good enough on name bank

--- src/Components/TeamSelection.jsx
+++ src/Components/TeamSelection.jsx
@@ -1,20 +1,48 @@
 import React from 'react';
 import TeamBox from './TeamBox';
 import Name from './Name';
+import Grid from "@material-ui/core/Grid";
+import {Card, CardContent, Container} from "@material-ui/core";
+
+const studentNames = [
+    "Daniel", "Abraham", "Patrick", "Zion",
+    "Bill", "Jim", "Billy", "Bob",
+    "Cory", "Josiah", "Ezekial", "Al",
+    "Fred", "Keaton", "Hector", "Susie",
+    "Rachael", "Taylor", "Jack", "Broheim",
+    "Jason", "Said", "Hank", "Sally",
+    "Renee", "Tim", "Chester", "Alex",
+    "Rick", "Lewis", "Jenna", "Ella",
+    "Charles", "Julie", "Marcus", "Barbara",
+    "Judy", "Kate", "Bradley", "Circle"
+];
+
+const letters = [...'ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
 
 function TeamSelection(props) {
     return (
-        <div className="flexbox">
-            <TeamBox id="box-1" className="board">
-                <Name id="name-1" className="card" draggable="true">
-                        <p>Card One</p>
+        <div>
+            <TeamBox id="box-1" className="nameBank">
+                        {/*<Name id="name-1" className="card" draggable="true">*/}
+                        {/*        <p>Card One</p>*/}
+                        {/*</Name>*/}
+                        {studentNames.map((studentName, index) =>
+                                <Name id={index} className="name" draggable="true">
+                                    <Card variant="outlined">
+                                        <CardContent>
+                                            {studentName}
+                                        </CardContent>
+                                    </Card>
+                                </Name>
+                        )}
+            </TeamBox>
+            <TeamBox id="box-2" className="teamTile">
+                <Name id="name-2" className="card" draggable="true">
+                    <p>Card Two</p>
                 </Name>
             </TeamBox>
-            <TeamBox id="box-2" className="board">
-            <Name id="name-2" className = "card" draggable="true">
-                <p>Card Two</p>
-            </Name>
-        </TeamBox>
+
+
         </div>
     );
 }
