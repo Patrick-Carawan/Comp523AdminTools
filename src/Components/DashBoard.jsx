705932bac3e878ee3503d71db2985bf3de581639
DanielWeber97 <daniel.weber.443@gmail.com>
2020-03-11 02:00:26 -0400

    set up side bar

--- src/Components/DashBoard.jsx
+++ src/Components/DashBoard.jsx
@@ -1,7 +1,120 @@
-import React, { useState } from "react";
+import React from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Proposals from '@material-ui/icons/Description';
+import MailIcon from '@material-ui/icons/Mail';
+import Team from '@material-ui/icons/Group';
+import Calendar from '@material-ui/icons/DateRange';
+import Deliverable from '@material-ui/icons/ListAlt';
 
-function DashBoard() {
-  return <div></div>;
+const drawerWidth = 240;
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        display: 'flex',
+    },
+    appBar: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth,
+    },
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+    },
+    drawerPaper: {
+        width: drawerWidth,
+    },
+    toolbar: theme.mixins.toolbar,
+    content: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.default,
+        padding: theme.spacing(3),
+    },
+}));
+
+export default function Dashboard() {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <CssBaseline/>
+            <AppBar position="fixed" className={classes.appBar}>
+                <Toolbar>
+                    <Typography variant="h6" noWrap>
+                        Permanent drawer
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                className={classes.drawer}
+                variant="permanent"
+                classes={{
+                    paper: classes.drawerPaper,
+                }}
+                anchor="left"
+            >
+                <div className={classes.toolbar}/>
+                <Divider/>
+                <List>
+                    <ListItem button key={1}>
+                        <ListItemIcon><MailIcon/></ListItemIcon>
+                        <ListItemText primary="Email Clients"/>
+                    </ListItem>
+                    <ListItem button key={2}>
+                        <ListItemIcon><Proposals/></ListItemIcon>
+                        <ListItemText primary="View Proposals"/>
+                    </ListItem>
+                    <Divider/>
+                    <ListItem button key={3} title="Change which students are in which teams">
+                        <ListItemIcon><Team/></ListItemIcon>
+                        <ListItemText primary="Manage Teams"/>
+                    </ListItem>
+                    <ListItem button key={4} title="Keep track of teams' progress in coach meetings">
+                        <ListItemIcon><Calendar/></ListItemIcon>
+                        <ListItemText primary="Track Coach Meetings"/>
+                    </ListItem>
+                    <ListItem button key={5} title="Give feedback on team deliverables">
+                        <ListItemIcon><Deliverable/></ListItemIcon>
+                        <ListItemText primary="Rate Deliverables"/>
+                    </ListItem>
+                </List>
+                <Divider/>
+            </Drawer>
+            <main className={classes.content}>
+                <div className={classes.toolbar}/>
+                <Typography paragraph>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                    ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
+                    facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
+                    gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
+                    donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+                    adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
+                    Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
+                    imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
+                    arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
+                    donec massa sapien faucibus et molestie ac.
+                </Typography>
+                <Typography paragraph>
+                    Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
+                    facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
+                    tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
+                    consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
+                    vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
+                    hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
+                    tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
+                    nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
+                    accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
+                </Typography>
+            </main>
+        </div>
+    );
 }
-
-export default DashBoard;
