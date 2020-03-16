e4f17741ae65be9b74635d0af9fe8f397fcee30b
Patrick Dane Carawan <pat1@live.unc.edu>
2020-03-16 14:35:53 -0400

    Minor formatting

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 let User = require('../models/user.model');
 
 router.route(`/students/:semester`).get((req, res) => {
-    User.find({ "semester": req.params.semester})
+    User.find({ "semester": req.params.semester })
         .then(users => res.json(users))
         .catch(err => res.status(400).json('Error: ' + err));
 });
