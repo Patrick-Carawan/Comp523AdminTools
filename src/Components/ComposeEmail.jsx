63b3608189c7c260d8a296a6523cdc605bb7c38d
DanielWeber97 <daniel.weber.443@gmail.com>
2020-03-21 20:18:33 -0400

    fixed background color

--- src/Components/ComposeEmail.jsx
+++ src/Components/ComposeEmail.jsx
@@ -25,7 +25,7 @@ const useStyles = makeStyles(theme => ({
     toolbar: theme.mixins.toolbar,
     content: {
         flexGrow: 1,
-        backgroundColor: theme.palette.background.default,
+        // backgroundColor: theme.palette.background.default,
         padding: theme.spacing(3),
     },
 }));
