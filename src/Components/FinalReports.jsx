11324eb9afdf7325178221e68463172f7226263f
DanielWeber97 <daniel.weber.443@gmail.com>
2020-03-31 21:58:38 -0400

    added dropdown

--- src/Components/FinalReports.jsx
+++ src/Components/FinalReports.jsx
@@ -3,6 +3,11 @@ import DashBoard from "./DashBoard";
 import Container from "@material-ui/core/Container";
 import {makeStyles} from "@material-ui/core/styles";
 import axios from 'axios';
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import Select from "@material-ui/core/Select";
+import {Input} from "@material-ui/icons";
+import MenuItem from "@material-ui/core/MenuItem";
 
 
 const useStyles = makeStyles(theme => ({
@@ -16,15 +21,30 @@ const useStyles = makeStyles(theme => ({
         padding: theme.spacing(3),
     },
 }));
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+    PaperProps: {
+        style: {
+            maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+            width: 250,
+        },
+    },
+};
 
 function FinalReports() {
     const classes = useStyles();
     const [teams, setTeams] = useState([]);
+    const [currentTeam, setCurrentTeam] = useState();
     useEffect(() => {
         axios.get(`http://localhost:5000/teams`).then((res) => {
             console.log(res['data']);
         })
     }, []);
+
+    const handleChange = (event) => {
+        setCurrentTeam(event.target.value);
+    };
     return (
         <div className={classes.root}>
             <DashBoard/>
@@ -32,6 +52,22 @@ function FinalReports() {
                 <div className={classes.toolbar}/>
                 <Container maxWidth="sm">
                     <h1>Select Team to View Reports</h1>
+                    <FormControl className={classes.formControl}>
+                        <InputLabel>Name</InputLabel>
+                        <Select
+                            multiple
+                            // value={personName}
+                            onChange={handleChange}
+                            input={<Input />}
+                            MenuProps={MenuProps}
+                        >
+                            {teams.map((teamItem) => (
+                                <MenuItem key={teamItem} value>
+                                    {name}
+                                </MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
                 </Container>
             </main>
         </div>);
