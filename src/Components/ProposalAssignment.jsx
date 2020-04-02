9c9a4d16e9e1df86d6173f414affb513f4f1ecd7
DanielWeber97 <daniel.weber.443@gmail.com>
2020-04-02 12:44:47 -0400

    created cards for each team

--- src/Components/ProposalAssignment.jsx
+++ src/Components/ProposalAssignment.jsx
@@ -3,6 +3,8 @@ import {makeStyles} from "@material-ui/core/styles";
 import DashBoard from "./DashBoard";
 import Container from "@material-ui/core/Container";
 import axios from 'axios';
+import {Card, CardContent} from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -14,6 +16,11 @@ const useStyles = makeStyles(theme => ({
         // backgroundColor: theme.palette.background.default,
         padding: theme.spacing(3),
     },
+    card:{
+        padding: '.5em',
+        marginBottom: '1em',
+        maxWidth: '60%'
+    }
 }));
 
 function ProposalAssignment(props) {
@@ -23,22 +30,32 @@ function ProposalAssignment(props) {
 
     useEffect(() => {
         axios.get(`http://localhost:5000/teams/Spring2020`).then(res => {
-            console.log('teams',res);
+            console.log('teams', res);
             setTeams(res['data']);
         });
-        axios.get(`http://localhost:5000/proposals`).then(res =>{
-            console.log('proposals',res);
-            setProposals(res['data']);
+        axios.get(`http://localhost:5000/proposals`).then(res => {
+            console.log('proposals', res);
+            let acceptedProps = res['data'].filter(proposal => proposal['status'] === "Accepted");
+            console.log(acceptedProps);
+            setProposals(acceptedProps);
         })
-    },[]);
+    }, []);
     return (
         <div className={classes.root}>
             <DashBoard/>
             <main className={classes.content}>
                 <div className={classes.toolbar}/>
-                <Container maxWidth="md">
-
-
+                <Container maxWidth="xs">
+                    <h1>Assign Projects to Teams</h1>
+                    {
+                        teams.map((team, i)=>
+                        <Card className={classes.card} key={i}>
+                            {/*<CardContent>*/}
+                                <Typography>{team.teamName}</Typography>
+                            {/*</CardContent>*/}
+                        </Card>
+                        )
+                    }
                 </Container>
             </main>
         </div>
