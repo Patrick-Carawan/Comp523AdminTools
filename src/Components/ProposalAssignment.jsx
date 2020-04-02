81a10b479665084634d94758982cc78398e00f35
DanielWeber97 <daniel.weber.443@gmail.com>
2020-04-02 16:11:29 -0400

    mapped selects work independently

--- src/Components/ProposalAssignment.jsx
+++ src/Components/ProposalAssignment.jsx
@@ -5,6 +5,10 @@ import Container from "@material-ui/core/Container";
 import axios from 'axios';
 import {Card, CardContent} from "@material-ui/core";
 import Typography from "@material-ui/core/Typography";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -19,27 +23,61 @@ const useStyles = makeStyles(theme => ({
     card:{
         padding: '.5em',
         marginBottom: '1em',
-        maxWidth: '60%'
+        // maxWidth: '60%'
+    },
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 120,
+        marginLeft: '5%'
     }
 }));
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+    PaperProps: {
+        style: {
+            maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+            width: 250,
+        },
+    },
+};
 
 function ProposalAssignment(props) {
     const classes = useStyles();
     const [teams, setTeams] = useState([]);
+    const [teamSelect, setTeamSelect] = useState([]);
     const [proposals, setProposals] = useState([]);
+    const [pairings, setPairings] = useState(new Map());
 
+    useEffect(()=> console.log(pairings),[pairings]);
     useEffect(() => {
         axios.get(`http://localhost:5000/teams/Spring2020`).then(res => {
             console.log('teams', res);
-            setTeams(res['data']);
+            const allTeams = res['data'];
+            let teamMap = new Map();
+            allTeams.forEach(team => teamMap.set(team,'No Project'));
+            setPairings(teamMap);
+            const fillArray = new Array(allTeams.length);
+            fillArray.fill(-1);
+            setTeamSelect(fillArray);
+            setTeams(allTeams);
         });
         axios.get(`http://localhost:5000/proposals`).then(res => {
             console.log('proposals', res);
             let acceptedProps = res['data'].filter(proposal => proposal['status'] === "Accepted");
-            console.log(acceptedProps);
             setProposals(acceptedProps);
+            console.log(acceptedProps);
+
         })
     }, []);
+
+    function handleChange(e,index) {
+        const copy = [...teamSelect];
+        copy.splice(index, 1, e.target.value);
+        setTeamSelect(copy);
+    }
+
     return (
         <div className={classes.root}>
             <DashBoard/>
@@ -48,10 +86,25 @@ function ProposalAssignment(props) {
                 <Container maxWidth="xs">
                     <h1>Assign Projects to Teams</h1>
                     {
-                        teams.map((team, i)=>
-                        <Card className={classes.card} key={i}>
+                        teams.map((team, index)=>
+                        <Card className={classes.card} key={index}>
                             {/*<CardContent>*/}
                                 <Typography>{team.teamName}</Typography>
+
+                            <FormControl className={classes.formControl}>
+                                <InputLabel>Select Project</InputLabel>
+                                <Select
+                                    value={teamSelect[index]}
+                                    onChange={(e)=>handleChange(e,index)}
+                                    MenuProps={MenuProps}
+                                >
+                                    {proposals.map((proposal, i) => (
+                                        <MenuItem key={i} value={proposal._id}>
+                                            {proposal.title}
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                            </FormControl>
                             {/*</CardContent>*/}
                         </Card>
                         )
