cd45ea3046f8caa52ad444d761c20deabc6cc36f
DanielWeber97 <daniel.weber.443@gmail.com>
2020-04-11 02:52:57 -0400

    set up to upload csv file, doesn't post to backend or display yet

--- src/Components/Roster.jsx
+++ src/Components/Roster.jsx
@@ -1,11 +1,13 @@
-import React, {useState} from 'react';
+import React from "react";
+import ReactDOM from "react-dom";
+import '../App.css'
+import CSVReader from "react-csv-reader";
 import {makeStyles} from "@material-ui/core/styles";
 import DashBoard from "./DashBoard";
 import Container from "@material-ui/core/Container";
-import {TextField} from "@material-ui/core";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
-import Card from "@material-ui/core/Card";
+
+const handleForce = (data) => console.log(data);
+
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -17,48 +19,29 @@ const useStyles = makeStyles((theme) => ({
         // backgroundColor: theme.palette.background.default,
         padding: theme.spacing(3),
     },
+    csvInput: {
+        padding: '10px',
+        display: 'block',
+        margin: '15px auto',
+        border: '1px solid #ccc',
+        borderRadius: '5px',
+    },
+    uploadContainer: {
+        textAlign: 'center',
+        padding: '15px',
+        margin: '10px auto',
+    }
 }));
 
-const dummyRoster = [
-    {onyen: 'dan97w', firstName: 'Daniel', lastName: 'Weber'}
-];
-
+const parseOptions = {
+    header: true,
+    dynamicTyping: true,
+    skipEmptyLines: true,
+    transformHeader: header => header.toLowerCase().replace(/\W/g, "_")
+};
 
 function Roster() {
     const classes = useStyles();
-    const [names, setNames] = useState([]);
-    const [onyens, setOnyens] = useState([]);
-
-    function onyensChange(e) {
-        let onyensArr = e.target.value.split('\n');
-        setOnyens(onyensArr)
-        // console.log(onyensArr)
-    }
-
-    function namesChange(e) {
-        let namesArr = e.target.value.split('\n');
-        setNames(namesArr);
-        // console.log(namesArr)
-    }
-
-    function submitRoster(){
-        let payload = [];
-        if (names.length === 0){
-            alert('Please enter names and onyens')
-        } else if(names.length !== onyens.length) {
-            alert('You must enter the same number of names and onyens');
-        } else{
-            names.forEach((name, index) =>{
-                let lastAndFirstName = name.split(',');
-                payload.push({
-                    firstName: lastAndFirstName[1],
-                    lastName: lastAndFirstName[0],
-                    onyen: onyens[index]
-                });
-            });
-            console.log(payload)
-        }
-    }
 
     return (
         <div className={classes.root}>
@@ -66,22 +49,17 @@ function Roster() {
             {console.log(window.innerHeight)}
             <main className={classes.content}>
                 <div className={classes.toolbar}/>
-                <Container maxWidth="md">
-
-                    <Card>
-
-                    </Card>
-                    <Button color="secondary" variant="contained">Add Roster</Button>
-                    <Typography>Type your roster here</Typography>
-                    <TextField value={names} onChange={(e)=>namesChange(e)} multiline variant="outlined"
-                               label="Names (Last, First)">
-
-                    </TextField>
-                    <TextField value={onyens} onChange={(e)=> onyensChange(e)} multiline variant="outlined"
-                               label="Corresponding Onyens">
-
-                    </TextField>
-                    <Button variant="outlined" onClick={submitRoster}>Submit Roster</Button>
+                <Container maxWidth="sm">
+                    <div className={classes.uploadContainer}>
+                    <p>Follow this example's formatting</p>
+                    <img alt="Example CSV"  src={require("../Images/exampleCSV.PNG")}/>
+                        <CSVReader
+                            cssClass="react-csv-input"
+                            label="Upload Roster CSV (comma delimited, NOT XSLX or XSL) here. "
+                            onFileLoaded={handleForce}
+                            parserOptions={parseOptions}
+                        />
+                    </div>
                 </Container>
             </main>
         </div>
@@ -89,3 +67,4 @@ function Roster() {
 }
 
 export default Roster;
+
